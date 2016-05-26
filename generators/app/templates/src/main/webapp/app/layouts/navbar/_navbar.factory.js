3cd4c44f29dcb76eaf3c2b5a8eaed8c983fa90bd
Castellanos Molina <jose.castellanos@autodesk.com>
2016-05-26 18:38:39 -0500

    Added more translation and tollbar

--- generators/app/templates/src/main/webapp/app/layouts/navbar/_navbar.factory.js
+++ generators/app/templates/src/main/webapp/app/layouts/navbar/_navbar.factory.js
@@ -4,99 +4,105 @@
 
   angular.module('<%=angularAppName%>')
     .factory('menu', [
-      '$location',
-      function ($location) {
+      '$location','Principal',
+      function ($location, Principal) {
 
         var sections = [];
 
-        sections.push({
-          name: 'Entities',
-          type: 'toggle',
-          pages: [
-            
-            // jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here
-          ]
-        });
+        if (Principal.isAuthenticated()) {
+          sections.push({
+            name: 'Entities',
+            type: 'toggle',
+            pages: [
+              
+              // jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here
+            ]
+          });
+        }
 
-        sections.push({
-          name: 'Account',
-          type: 'toggle',
-          pages: [{
-            name: 'Settings',
-            type: 'link',
-            state: 'settings',
-            icon: 'settings'
-          }, {
-            name: 'Password',
-            state: 'password',
-            type: 'link',
-            icon: 'verified_user'
-          },
-            {
-              name: 'Sign out',
-              state: 'munchies.donuts',
-              type: 'link',
-              icon: 'logout'
-            },
-            {
-              name: 'Sign in',
-              state: 'munchies.donuts',
-              type: 'link',
-              icon: 'login'
-            },
-            {
-              name: 'Register',
-              state: 'register',
-              type: 'link',
-              icon: 'person_add'
-            }]
-        });
+        if (Principal.isAuthenticated()) {
+            sections.push({
+              name: 'Account',
+              type: 'toggle',
+              pages: [{
+                name: 'Settings',
+                type: 'link',
+                state: 'settings',
+                icon: 'settings'
+              }, {
+                name: 'Password',
+                state: 'password',
+                type: 'link',
+                icon: 'verified_user'
+              },
+                {
+                  name: 'Sign out',
+                  state: 'munchies.donuts',
+                  type: 'link',
+                  icon: 'logout'
+                },
+                {
+                  name: 'Sign in',
+                  state: 'munchies.donuts',
+                  type: 'link',
+                  icon: 'login'
+                },
+                {
+                  name: 'Register',
+                  state: 'register',
+                  type: 'link',
+                  icon: 'person_add'
+                }]
+            });
+        }
         
-        sections.push({
-          name: 'Administration',
-          type: 'toggle',
-          pages: [{
-            name: 'User management',
-            type: 'link',
-            state: 'user-management',
-            icon: 'person'
-          }, {
-            name: 'Metrics',
-            state: 'jhi-metrics',
-            type: 'link',
-            icon: 'av_timer'
-          },
-            {
-              name: 'Health',
-              state: 'jhi-health',
+       if (Principal.hasAuthority('ROLE_ADMIN')) { 
+          sections.push({
+            name: 'Administration',
+            type: 'toggle',
+            pages: [{
+              name: 'User management',
               type: 'link',
-              icon: 'healing'
+              state: 'user-management',
+              icon: 'person'
+            }, {
+              name: 'Metrics',
+              state: 'jhi-metrics',
+              type: 'link',
+              icon: 'av_timer'
             },
-            {
-              name: 'Configuration',
-              state: 'jhi-configuration',
-              type: 'link',
-              icon: 'settings'
-            },
-            {
-              name: 'Audits',
-              state: 'audits',
-              type: 'link',
-              icon: 'assignment'
-            },
-            {
-              name: 'logs',
-              state: 'logss',
-              type: 'link',
-              icon: 'receipt'
-            },
-            {
-              name: 'API',
-              state: 'docs',
-              type: 'link',
-              icon: 'my_library_books'
-            }]
-        });
+              {
+                name: 'Health',
+                state: 'jhi-health',
+                type: 'link',
+                icon: 'healing'
+              },
+              {
+                name: 'Configuration',
+                state: 'jhi-configuration',
+                type: 'link',
+                icon: 'settings'
+              },
+              {
+                name: 'Audits',
+                state: 'audits',
+                type: 'link',
+                icon: 'assignment'
+              },
+              {
+                name: 'logs',
+                state: 'logss',
+                type: 'link',
+                icon: 'receipt'
+              },
+              {
+                name: 'API',
+                state: 'docs',
+                type: 'link',
+                icon: 'my_library_books'
+              }]
+          });
+       }
 
         var self;
 
