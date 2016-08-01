b044cff39a61b0108190c674c90af88bfc68f918
Jose Castellanos <jose.castellanos.molina@gmail.com>
2016-08-01 08:27:17 -0500

    added register and password

--- generators/app/templates/src/main/webapp/app/components/login/_login.service.js
+++ generators/app/templates/src/main/webapp/app/components/login/_login.service.js
@@ -34,6 +34,66 @@
                       
         }
 
+        function openResetPassword(ev) {
+                var useFullScreen = ($mdMedia('sm') || $mdMedia('xs') );
+                var self = this;
+                
+                $mdDialog.show({
+                    templateUrl: 'app/account/password/password.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    controller: 'PasswordController',
+                    controllerAs: 'vm',
+                    clickOutsideToClose: true,
+                    fullscreen: useFullScreen
+                }).then( function() {
+
+                }, function() {
+
+                });
+                      
+        }
+
+        function register(ev) {
+                var useFullScreen = ($mdMedia('sm') || $mdMedia('xs') );
+                var self = this;
+                
+                $mdDialog.show({
+                    templateUrl: 'app/account/register/register.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    controller: 'RegisterController',
+                    controllerAs: 'vm',
+                    clickOutsideToClose: true,
+                    fullscreen: useFullScreen
+                }).then( function() {
+
+                }, function() {
+
+                });
+                      
+        }
+
+        function openActivate(ev) {
+                var useFullScreen = ($mdMedia('sm') || $mdMedia('xs') );
+                var self = this;
+                
+                $mdDialog.show({
+                    templateUrl: 'app/account/activate/activate.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    controller: 'ActivationController',
+                    controllerAs: 'vm',
+                    clickOutsideToClose: true,
+                    fullscreen: useFullScreen
+                }).then( function() {
+
+                }, function() {
+
+                });
+                      
+        }
+
 
         
     }
